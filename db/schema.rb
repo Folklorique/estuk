f839ee07e39a81c52b4fda6f9fa91f433c22bfa2
Anthony <anthony.lely@me.com>
2015-06-30 19:59:01 +0200

    Added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629171133) do
+ActiveRecord::Schema.define(version: 20150630175407) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "author"
     t.text     "description"
     t.integer  "price"
-    t.boolean  "availability", default: true
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.boolean  "availability",           default: true
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "ressource_file_name"
+    t.string   "ressource_content_type"
+    t.integer  "ressource_file_size"
+    t.datetime "ressource_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
